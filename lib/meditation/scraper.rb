cbc09105730d70e2f3cc79261bce5ee5b64b88ac
miriam <miriam5000@gmail.com>
2017-10-26 22:31:48 -0400

    creating methods in classes

--- lib/meditation/scraper.rb
+++ lib/meditation/scraper.rb
@@ -1,3 +1,22 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 class Meditation::Scraper
+ 	
+ 	@@all = []
+
+ 	def get_page
+ 		Nokogiri
+ 	end
+
+ 	def find_meditation
+ 		x = self.get_page... html selectors 
+ 	end 
+
+ 	def make_object
+ 		x.each_with_index
+ 	end 
+
 
 end 
--- lib/meditation/your_meditation.rb
+++ lib/meditation/your_meditation.rb
@@ -1,3 +1,23 @@
 class Meditation::Your_meditation
+@@all = []
+
+	def self.new_from_index
+		self.new(
+				html here 
+
+			)
+		@@all << self
+	end 
+
+	def initialize(talk_name = nil, 
+	 @talk_name = talk_name 
+	 etc 
+	end 
+
+	def self.all
+		@@all
+	end 
+
+
 
 end
