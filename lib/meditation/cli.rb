fe1cd4e625165612f8632205e877062d6cf6d4b9
miriam <miriam5000@gmail.com>
2017-10-27 15:22:41 -0400

    cli, calling in data

--- lib/meditation/cli.rb
+++ lib/meditation/cli.rb
@@ -1,40 +1,50 @@
 class Meditation::CLI
 
- def call
- 	Meditation::Scraper.new.make_object
- 	puts "Welcome, and get ready to meditate."
- 	menu
- end 
+	 def call
+	 	Meditation::Scraper.new.make_object
+	 	puts "Welcome, and get ready to meditate."
+	 	menu
+	 end 
 
- def menu
- 	puts "@@@@@@@@@@\n"
- 	display_meditations
- 	puts "Here are ten meditations. Which number would you like to see?"
- 	choice = gets.strip.to_i
+	 def menu
+	 	puts "@@@@@@@@@@\n"
+	 	display_meditations
+	 	puts "Here are ten meditations. Which number would you like to see?"
+	 	choice = gets.strip.to_i
 
- 	display_chosen_meditation(choice)
- end 
+	 	display_chosen_meditation(choice)
 
- def display_meditations
- 	puts "@@@@@@@@@@\n"
- 	display = Meditation::Scraper.new.make_object
- end 
+	 	choose_again
+	 end 
 
- def display_chosen_meditation(choice)
- 	chosen_meditation = Meditation::Scraper.find
+	 def display_meditations
+	 	puts "@@@@@@@@@@\n"
+	 	display = Meditation::Scraper.new.make_object
+	 end 
 
- end
+	 def display_chosen_meditation(choice)
+	 	#chosen_meditation = Meditation::Scraper.find(choice)
+	 	puts "@@@@@@@@@@\n"
+	 	puts "Title:  	#{.title}"
+	 	puts "Teacher:     #{.teacher}"
+	 	puts "Length:      #{.length}"
+	 	puts "Stream: 		#{.stream}"
 
- def choose_again
- 	puts "Would you like to choose another? Press y, or x to exit."
- 	input = get.strip.downcase
 
- 	if input == "y"
- 		menu
- 	else
- 		puts "Have a calm and grounded day."
- 	end
- end 
+
+	 end
+
+	 def choose_again
+	 	puts "@@@@@@@@@@\n"
+	 	puts "Would you like to choose another? Press y, or x to exit."
+	 	next = get.strip.downcase
+
+	 	if next == "y"
+	 		menu
+	 	else
+	 		puts "Have a calm and grounded day."
+	 	end
+	 end 
  
 
 end 
