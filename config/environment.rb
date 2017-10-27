e27e7da174dab50da7378add506ae90f564fe501
miriam <miriam5000@gmail.com>
2017-10-26 22:10:37 -0400

    created 3 classes and set requirments, created config/environment

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+require 'bundler' #this loads bundler
+Bundler.require   #this uses bundler to require gems from the gemfile 
+require "require_all"
+
+require_all './lib' #this loads all the files 
---
+++ lib/meditation/your_meditation.rb
@@ -0,0 +1,1 @@
+class Meditation::Your_meditation
